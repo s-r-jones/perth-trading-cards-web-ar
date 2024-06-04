21f6a65d19ef2f8c5c4e4b86d03fde13cad21b4d
Sam Jones <sam@refractar.com>
2024-06-04 11:06:05 -0600

    closers

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -9,53 +9,64 @@ export const Loading = ({ onStartButtonClick, camKitLoaded }: Props) => {
   const [view, setView] = useState(0);
   return (
     <div className="loading">
-      <section>
-        <h2>How to use AR</h2>
+      <div className="container">
         {view === 0 && (
-          <ol type="1">
-            <li>
-              <span>1</span>
-              <img src="magnifying-glass.png" />
-              <p>Allow permissions & Aim your phone at the ground</p>
-            </li>
+          <section>
+            <h2>How to use AR</h2>
 
-            <li>
-              <span>2</span>
-              <img src="camera.png" />
-              <p>Aim your camera at the trading card</p>
-            </li>
-            <li>
-              <span>3</span>
-              <img src="tap.png" />
-              <p>Tap the trading card to collect!</p>
-            </li>
-          </ol>
+            <ol type="1">
+              <li>
+                <span>1</span>
+                <img src="magnifying-glass.png" />
+                <p>Allow permissions & Aim your phone at the ground</p>
+              </li>
+
+              <li>
+                <span>2</span>
+                <img src="camera.png" />
+                <p>Aim your camera at the trading card</p>
+              </li>
+              <li>
+                <span>3</span>
+                <img src="tap.png" />
+                <p>Tap the trading card to collect!</p>
+              </li>
+            </ol>
+          </section>
         )}
         {view === 1 && (
-          <ol type="1">
-            <li>
-              <span>1</span>
-              <img src="magnifying-glass.png" />
-              <p>Allow permissions & Aim your phone at the ground</p>
-            </li>
+          <section>
+            <h2>Prizes</h2>
+            <h3>Collect cards to win these prices</h3>
 
-            <li>
-              <span>2</span>
-              <img src="camera.png" />
-              <p>Aim your camera at the trading card</p>
-            </li>
-            <li>
-              <span>3</span>
-              <img src="tap.png" />
-              <p>Tap the trading card to collect!</p>
-            </li>
-          </ol>
+            <ol
+              type="1"
+              className="prize-list"
+            >
+              <li>
+                <img src="voucher-10.png" />
+                <h4>$10 Gift Voucher</h4>
+                <span>5 players</span>
+              </li>
+
+              <li>
+                <img src="voucher-25.png" />
+                <h4>$25 Gift Voucher</h4>
+                <span>15 players</span>
+              </li>
+              <li>
+                <img src="jersey.png" />
+                <h4>2024 Perth Redbacks Signed Singlet</h4>
+                <span>30 players</span>
+              </li>
+              <li>
+                <img src="training.png" />
+                <h4>1 Hour Player Training Session</h4>
+                <span>grand prize draw</span>
+              </li>
+            </ol>
+          </section>
         )}
-
-        <div className="loading-instruction-step">
-          <span />
-          <span />
-        </div>
         <button
           //disabled={!camKitLoaded}
           onClick={() => {
@@ -69,7 +80,11 @@ export const Loading = ({ onStartButtonClick, camKitLoaded }: Props) => {
         >
           {view === 1 ? "Start" : "Next"}
         </button>
-      </section>
+        <div className="loading-instruction-step">
+          <span className={view === 0 ? "active" : ""} />
+          <span className={view === 1 ? "active" : ""} />
+        </div>
+      </div>
     </div>
   );
 };
