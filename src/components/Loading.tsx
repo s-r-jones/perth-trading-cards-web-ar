2b99c537d3783e9ded66ac9f5f96840d05469461
Sam Jones <sam@refractar.com>
2024-06-04 10:10:18 -0600

    add some css and routing

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -11,28 +11,53 @@ export const Loading = ({ onStartButtonClick, camKitLoaded }: Props) => {
     <div className="loading">
       <section>
         <h2>How to use AR</h2>
-        <ol type="1">
-          <li>
-            <span>1</span>
-            <img src="magnifying-glass.png" />
-            <p>Allow permissions & Aim your phone at the ground</p>
-          </li>
+        {view === 0 && (
+          <ol type="1">
+            <li>
+              <span>1</span>
+              <img src="magnifying-glass.png" />
+              <p>Allow permissions & Aim your phone at the ground</p>
+            </li>
 
-          <li>
-            <span>2</span>
-            <img src="camera.png" />
-            <p>Aim your camera at the trading card</p>
-          </li>
-          <li>
-            <span>3</span>
-            <img src="tap.png" />
-            <p>Tap the trading card to collect!</p>
-          </li>
-        </ol>
-        <br />
+            <li>
+              <span>2</span>
+              <img src="camera.png" />
+              <p>Aim your camera at the trading card</p>
+            </li>
+            <li>
+              <span>3</span>
+              <img src="tap.png" />
+              <p>Tap the trading card to collect!</p>
+            </li>
+          </ol>
+        )}
+        {view === 1 && (
+          <ol type="1">
+            <li>
+              <span>1</span>
+              <img src="magnifying-glass.png" />
+              <p>Allow permissions & Aim your phone at the ground</p>
+            </li>
 
+            <li>
+              <span>2</span>
+              <img src="camera.png" />
+              <p>Aim your camera at the trading card</p>
+            </li>
+            <li>
+              <span>3</span>
+              <img src="tap.png" />
+              <p>Tap the trading card to collect!</p>
+            </li>
+          </ol>
+        )}
+
+        <div className="loading-instruction-step">
+          <span />
+          <span />
+        </div>
         <button
-          disabled={!camKitLoaded}
+          //disabled={!camKitLoaded}
           onClick={() => {
             if (view === 1) {
               onStartButtonClick();
@@ -42,7 +67,7 @@ export const Loading = ({ onStartButtonClick, camKitLoaded }: Props) => {
             setView(1);
           }}
         >
-          `{view === 1 ? "Start" : "Next"}`
+          {view === 1 ? "Start" : "Next"}
         </button>
       </section>
     </div>
